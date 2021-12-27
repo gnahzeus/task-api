50fbc937353ab0dd81aa66b585be2ce6da6b62d9
gnahzeus <zhangyue1689@gmail.com>
2021-12-27 16:50:19 +0800

    add welcome

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   scope '/api/version1' do
     resources :tasks
   end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index' 
 end
