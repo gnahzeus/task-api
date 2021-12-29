28aeb6f7f604228d185aebccbffb3fcca6767f7d
gnahzeus <zhangyue1689@gmail.com>
2021-12-29 10:01:07 +0800

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   scope '/api/version1' do
     resources :tasks
   end
+  # global options responder -> makes sure OPTION request for CORS endpoints work
+  match '*path', via: [:options], to: lambda {|_| [204, { 'Content-Type' => 'text/plain' }]}
   get "/", to: "tasks#index"
   root to: 'tasks#index'
 end
